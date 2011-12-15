c9c937504b67e4e4a423b268dba3e55578af0d97
Scott Sims <ssims@homeaway.com>
2011-12-15 14:30:36 -0600

    updating spec helper for webdriver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,15 @@ require 'bundler/setup'
 require 'automata'
 
 RSpec.configure do |config|
-  config.before(:each) do
+   config.before(:each) do
     Log.debug "Example Group: #{example.example_group.description}"
     Log.debug "Example: #{example.description}"
-    Log.debug "Example Location: #{example.location}"
   end
   config.after(:each) do
-      browser.close_current_browser_session unless browser.nil?
+    browser.close_current_browser_session unless (browser.nil? || browser.session_id.nil?)
+    driver.quit unless driver.nil?
   end
-  config.include(Browser::Driver)
   config.include(Browser::DomainDiscovery)
+  config.include(Browser::Driver)
   config.include(TestUtilities)
-  config.include(XmlHelper)
 end
