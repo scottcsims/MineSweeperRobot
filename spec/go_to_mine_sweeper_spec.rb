60a8d26e6e51684ddc7abfcd546f86e5c1d08e37
Scott Sims <ssims@homeaway.com>
2012-03-22 19:02:38 -0500

    refactoring mine sweeper spec

--- spec/go_to_mine_sweeper_spec.rb
+++ spec/go_to_mine_sweeper_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 describe MineSweeper do
+  before(:each)do
+    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html?preset=beginner"
+  end
+  let(:mine_sweeper){MineSweeper.new(driver)}
+  let(:center_coords){"g1r5c5"}
   #Classes
   #unclicked
   #mines #
@@ -7,62 +12,43 @@ describe MineSweeper do
   #id is location and class attribute is state
   #columns and rows start at 0
   it "should find the rows and columns" do
-    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html"
-    mine_sweeper =MineSweeper.new(driver)
-    mine_sweeper.rows.should == 16
-    mine_sweeper.columns.should == 30
-    mine_sweeper.cells.should == 480
+    mine_sweeper.rows.should == 9
+    mine_sweeper.columns.should == 9
+    mine_sweeper.cells.should == 81
   end
   it "should click a block" do
-    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html"
-    cell=driver.find_element(:id => "g1r8c15")
+    cell=driver.find_element(:id => "g1r1c8")
     cell.click
     puts cell.attribute("class")
     cell.attribute("class").should(include 'mines')
   end
   it "should store the coords and the mines" do
-    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html"
-    center_coords = "g1r8c15"
-    mine_sweeper =MineSweeper.new(driver)
     mine_sweeper.click_block center_coords
     mine_sweeper.mines[center_coords].should_not be_nil
     center_mine_count =driver.find_element(:id => center_coords).attribute("class").gsub!("mines", "").to_i
     mine_sweeper.mines[center_coords].should == center_mine_count
   end
   it "should flag a block" do
-    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html"
-    center_coords = "g1r8c15"
-    mine_sweeper =MineSweeper.new(driver)
-    mine_sweeper.mark_block "g1r8c15"
+    mine_sweeper.mark_block center_coords
     driver.find_element(:id => center_coords).attribute("class").should == "marked"
   end
   it "should find clicked blocks" do
-    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html"
-    mine_sweeper =MineSweeper.new(driver)
     3.times do
-      mine_sweeper.click_block "g1r#{rand(16)}c#{rand(30)}"
+      mine_sweeper.click_block "g1r#{rand(9)}c#{rand(9)}"
     end
     mine_sweeper.clicked_blocks.should_not be_nil
   end
   it "should get status" do
-    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html"
-    mine_sweeper =MineSweeper.new(driver)
     mine_sweeper.status.should == "alive"
   end
   it "should check alive" do
-    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html"
-    mine_sweeper =MineSweeper.new(driver)
     mine_sweeper.check_alive
   end
   it "should fail if not alive" do
-    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html"
-    mine_sweeper =MineSweeper.new(driver)
     mine_sweeper.should_receive(:status).and_return(nil)
     lambda { mine_sweeper.check_alive }.should(raise_exception("not alive"))
   end
   it "should have 1s" do
-    launch_web_driver "file://localhost/Users/ssims/RubymineProjects/minesweeper.github.com/index.html?preset=beginner"
-    mine_sweeper =MineSweeper.new(driver)
     mine_sweeper.click_block("g1r0c0")
     mine_sweeper.click_block("g1r8c0")
     mine_sweeper.click_block("g1r0c8")
@@ -73,13 +59,8 @@ describe MineSweeper do
     ones[0].attribute("id").should(include("g"))
     ones[0].attribute("id").should(include("r"))
     ones[0].attribute("id").should(include("c"))
-
-
   end
-  it "should find a clicked block with 1 mine and one unclicked block" do
-    #ahead and behind
-    #above below
-    #forward diagonal
-    #backward diagonal
+  it "should find a 1 touching one unclicked block" do
+    mine_sweeper
   end
 end
